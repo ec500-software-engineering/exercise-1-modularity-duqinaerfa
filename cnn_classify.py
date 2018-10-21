b335f2a3a3b2d57631d196c11343a8cd0222911b
duqinaerfa <36396904+duqinaerfa@users.noreply.github.com>
2018-10-21 16:52:04 -0400

    Update cnn_classify.py
    modify

--- cnn_classify.py
+++ cnn_classify.py
@@ -1,117 +1,133 @@
-from skimage import io, transform
+from skimage import io,transform
 import glob
 import os
 import tensorflow as tf
 import numpy as np
 import time
 
-path = '/Users/duqinmei/flowers/'
+path='/Users/duqinmei/flowers/'
+model_path='/Users/duqinmei/flowers/model.ckpt'
 
 # resize all pictures
-w = 100
-h = 100
-c = 3
+w=100
+h=100
+c=3
+
 
 # read pictures
 def read_img(path):
-    cate = [path + '/' + x for x in os.listdir(path) if os.path.isdir(path + '/' + x)]
-    imgs = []
-    labels = []
-    for idx, folder in enumerate(cate):
-        for im in glob.glob(folder + '/*.jpg'):
-            print('reading the images:%s' % (im))
-            img = io.imread(im)
-            img = transform.resize(img, (w, h))
+    cate=[path+x for x in os.listdir(path) if os.path.isdir(path+x)]
+    imgs=[]
+    labels=[]
+    for idx,folder in enumerate(cate):
+        for im in glob.glob(folder+'/*.jpg'):
+            print('reading the images:%s'%(im))
+            img=io.imread(im)
+            img=transform.resize(img,(w,h))
             imgs.append(img)
             labels.append(idx)
-    return np.asarray(imgs, np.float32), np.asarray(labels, np.int32)
+    return np.asarray(imgs,np.float32),np.asarray(labels,np.int32)
+data,label=read_img(path)
 
-
-data, label = read_img(path)
 
 # disrupt order
-num_example = data.shape[0]
-arr = np.arange(num_example)
+num_example=data.shape[0]
+arr=np.arange(num_example)
 np.random.shuffle(arr)
-data = data[arr]
-label = label[arr]
+data=data[arr]
+label=label[arr]
 
-# divide data
-ratio = 0.8
-s = np.int(num_example * ratio)
-x_train = data[:s]
-y_train = label[:s]
-x_val = data[s:]
-y_val = label[s:]
 
-# internet
-x = tf.placeholder(tf.float32, shape=[None, w, h, c], name='x')
-y_ = tf.placeholder(tf.int32, shape=[None, ], name='y_')
+# divide data into train&val
+ratio=0.8
+s=np.int(num_example*ratio)
+x_train=data[:s]
+y_train=label[:s]
+x_val=data[s:]
+y_val=label[s:]
 
-# first convolutional layer（100——>50)
-conv1 = tf.layers.conv2d(
-    inputs=x,
-    filters=32,
-    kernel_size=[5, 5],
-    padding="same",
-    activation=tf.nn.relu,
-    kernel_initializer=tf.truncated_normal_initializer(stddev=0.01))
-pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+#-----------------INTERNET----------------------
+x=tf.placeholder(tf.float32,shape=[None,w,h,c],name='x')
+y_=tf.placeholder(tf.int32,shape=[None,],name='y_')
 
-# second(50->25)
-conv2 = tf.layers.conv2d(
-    inputs=pool1,
-    filters=64,
-    kernel_size=[5, 5],
-    padding="same",
-    activation=tf.nn.relu,
-    kernel_initializer=tf.truncated_normal_initializer(stddev=0.01))
-pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+def inference(input_tensor, train, regularizer):
+    with tf.variable_scope('layer1-conv1'):
+        conv1_weights = tf.get_variable("weight",[5,5,3,32],initializer=tf.truncated_normal_initializer(stddev=0.1))
+        conv1_biases = tf.get_variable("bias", [32], initializer=tf.constant_initializer(0.0))
+        conv1 = tf.nn.conv2d(input_tensor, conv1_weights, strides=[1, 1, 1, 1], padding='SAME')
+        relu1 = tf.nn.relu(tf.nn.bias_add(conv1, conv1_biases))
 
-# third(25->12)
-conv3 = tf.layers.conv2d(
-    inputs=pool2,
-    filters=128,
-    kernel_size=[3, 3],
-    padding="same",
-    activation=tf.nn.relu,
-    kernel_initializer=tf.truncated_normal_initializer(stddev=0.01))
-pool3 = tf.layers.max_pooling2d(inputs=conv3, pool_size=[2, 2], strides=2)
+    with tf.name_scope("layer2-pool1"):
+        pool1 = tf.nn.max_pool(relu1, ksize = [1,2,2,1],strides=[1,2,2,1],padding="VALID")
 
-# forth(12->6)
-conv4 = tf.layers.conv2d(
-    inputs=pool3,
-    filters=128,
-    kernel_size=[3, 3],
-    padding="same",
-    activation=tf.nn.relu,
-    kernel_initializer=tf.truncated_normal_initializer(stddev=0.01))
-pool4 = tf.layers.max_pooling2d(inputs=conv4, pool_size=[2, 2], strides=2)
+    with tf.variable_scope("layer3-conv2"):
+        conv2_weights = tf.get_variable("weight",[5,5,32,64],initializer=tf.truncated_normal_initializer(stddev=0.1))
+        conv2_biases = tf.get_variable("bias", [64], initializer=tf.constant_initializer(0.0))
+        conv2 = tf.nn.conv2d(pool1, conv2_weights, strides=[1, 1, 1, 1], padding='SAME')
+        relu2 = tf.nn.relu(tf.nn.bias_add(conv2, conv2_biases))
 
-re1 = tf.reshape(pool4, [-1, 6 * 6 * 128])
+    with tf.name_scope("layer4-pool2"):
+        pool2 = tf.nn.max_pool(relu2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
 
-# layer
-dense1 = tf.layers.dense(inputs=re1,
-                         units=1024,
-                         activation=tf.nn.relu,
-                         kernel_initializer=tf.truncated_normal_initializer(stddev=0.01),
-                         kernel_regularizer=tf.contrib.layers.l2_regularizer(0.003))
-dense2 = tf.layers.dense(inputs=dense1,
-                         units=512,
-                         activation=tf.nn.relu,
-                         kernel_initializer=tf.truncated_normal_initializer(stddev=0.01),
-                         kernel_regularizer=tf.contrib.layers.l2_regularizer(0.003))
-logits = tf.layers.dense(inputs=dense2,
-                         units=5,
-                         activation=None,
-                         kernel_initializer=tf.truncated_normal_initializer(stddev=0.01),
-                         kernel_regularizer=tf.contrib.layers.l2_regularizer(0.003))
-# ---------------------------internet end ---------------------------
+    with tf.variable_scope("layer5-conv3"):
+        conv3_weights = tf.get_variable("weight",[3,3,64,128],initializer=tf.truncated_normal_initializer(stddev=0.1))
+        conv3_biases = tf.get_variable("bias", [128], initializer=tf.constant_initializer(0.0))
+        conv3 = tf.nn.conv2d(pool2, conv3_weights, strides=[1, 1, 1, 1], padding='SAME')
+        relu3 = tf.nn.relu(tf.nn.bias_add(conv3, conv3_biases))
 
-loss = tf.losses.sparse_softmax_cross_entropy(labels=y_, logits=logits)
-train_op = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
-correct_prediction = tf.equal(tf.cast(tf.argmax(logits, 1), tf.int32), y_)
-acc = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    with tf.name_scope("layer6-pool3"):
+        pool3 = tf.nn.max_pool(relu3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+
+    with tf.variable_scope("layer7-conv4"):
+        conv4_weights = tf.get_variable("weight",[3,3,128,128],initializer=tf.truncated_normal_initializer(stddev=0.1))
+        conv4_biases = tf.get_variable("bias", [128], initializer=tf.constant_initializer(0.0))
+        conv4 = tf.nn.conv2d(pool3, conv4_weights, strides=[1, 1, 1, 1], padding='SAME')
+        relu4 = tf.nn.relu(tf.nn.bias_add(conv4, conv4_biases))
+
+    with tf.name_scope("layer8-pool4"):
+        pool4 = tf.nn.max_pool(relu4, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+        nodes = 6*6*128
+        reshaped = tf.reshape(pool4,[-1,nodes])
+
+    with tf.variable_scope('layer9-fc1'):
+        fc1_weights = tf.get_variable("weight", [nodes, 1024],
+                                      initializer=tf.truncated_normal_initializer(stddev=0.1))
+        if regularizer != None: tf.add_to_collection('losses', regularizer(fc1_weights))
+        fc1_biases = tf.get_variable("bias", [1024], initializer=tf.constant_initializer(0.1))
+
+        fc1 = tf.nn.relu(tf.matmul(reshaped, fc1_weights) + fc1_biases)
+        if train: fc1 = tf.nn.dropout(fc1, 0.5)
+
+    with tf.variable_scope('layer10-fc2'):
+        fc2_weights = tf.get_variable("weight", [1024, 512],
+                                      initializer=tf.truncated_normal_initializer(stddev=0.1))
+        if regularizer != None: tf.add_to_collection('losses', regularizer(fc2_weights))
+        fc2_biases = tf.get_variable("bias", [512], initializer=tf.constant_initializer(0.1))
+
+        fc2 = tf.nn.relu(tf.matmul(fc1, fc2_weights) + fc2_biases)
+        if train: fc2 = tf.nn.dropout(fc2, 0.5)
+
+    with tf.variable_scope('layer11-fc3'):
+        fc3_weights = tf.get_variable("weight", [512, 5],
+                                      initializer=tf.truncated_normal_initializer(stddev=0.1))
+        if regularizer != None: tf.add_to_collection('losses', regularizer(fc3_weights))
+        fc3_biases = tf.get_variable("bias", [5], initializer=tf.constant_initializer(0.1))
+        logit = tf.matmul(fc2, fc3_weights) + fc3_biases
+
+    return logit
+
+#---------------------------INTERNET ENDS---------------------------
+regularizer = tf.contrib.layers.l2_regularizer(0.0001)
+logits = inference(x,False,regularizer)
+
+# for call model later
+b = tf.constant(value=1,dtype=tf.float32)
+logits_eval = tf.multiply(logits,b,name='logits_eval')
+
+loss=tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=y_)
+train_op=tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
+correct_prediction = tf.equal(tf.cast(tf.argmax(logits,1),tf.int32), y_)
+acc= tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 
 # obtain data
@@ -130,31 +146,28 @@ def minibatches(inputs=None, targets=None, batch_size=None, shuffle=False):
 
 # train and validate
 
-n_epoch = 10
-batch_size = 64
-sess = tf.InteractiveSession()
+n_epoch=10
+batch_size=64
+saver=tf.train.Saver()
+sess=tf.Session()
 sess.run(tf.global_variables_initializer())
 for epoch in range(n_epoch):
     start_time = time.time()
 
-    # training
+    #training
     train_loss, train_acc, n_batch = 0, 0, 0
     for x_train_a, y_train_a in minibatches(x_train, y_train, batch_size, shuffle=True):
-        _, err, ac = sess.run([train_op, loss, acc], feed_dict={x: x_train_a, y_: y_train_a})
-        train_loss += err;
-        train_acc += ac;
-        n_batch += 1
-    print("   train loss: %f" % (train_loss / n_batch))
-    print("   train acc: %f" % (train_acc / n_batch))
+        _,err,ac=sess.run([train_op,loss,acc], feed_dict={x: x_train_a, y_: y_train_a})
+        train_loss += err; train_acc += ac; n_batch += 1
+    print("   train loss: %f" % (np.sum(train_loss)/ n_batch))
+    print("   train acc: %f" % (np.sum(train_acc)/ n_batch))
 
-    # validation
+    #validation
     val_loss, val_acc, n_batch = 0, 0, 0
     for x_val_a, y_val_a in minibatches(x_val, y_val, batch_size, shuffle=False):
-        err, ac = sess.run([loss, acc], feed_dict={x: x_val_a, y_: y_val_a})
-        val_loss += err;
-        val_acc += ac;
-        n_batch += 1
-    print("   validation loss: %f" % (val_loss / n_batch))
-    print("   validation acc: %f" % (val_acc / n_batch))
-
+        err, ac = sess.run([loss,acc], feed_dict={x: x_val_a, y_: y_val_a})
+        val_loss += err; val_acc += ac; n_batch += 1
+    print("   validation loss: %f" % (np.sum(val_loss)/ n_batch))
+    print("   validation acc: %f" % (np.sum(val_acc)/ n_batch))
+saver.save(sess,model_path)
 sess.close()
